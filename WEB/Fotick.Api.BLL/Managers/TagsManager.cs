9d8dfa5f41f3d63f28846beda4a5c7edd1577530
yevheniyStepaniuk <eugenestepanjuk97@gmail.com>
2017-05-13 22:14:22 +0300

    Added clarifai credential, started develop tags manager

---
+++ WEB/Fotick.Api.BLL/Managers/TagsManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fotick.Api.BLL.Managers
+{
+    public class TagsManager
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IImageRepository _imageRepository;
+    }
+}
