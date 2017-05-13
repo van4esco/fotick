c9021842a9ed6b533ff1fa6621464079da4b6720
yevheniyStepaniuk <eugenestepanjuk97@gmail.com>
2017-05-13 14:53:46 +0300

    Added BLL

---
+++ WEB/Fotick.Api.BLL/Extensions/ServiceCollectionsExtension.cs
@@ -0,0 +1,19 @@
+﻿using Fotick.Api.DAL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fotick.Api.BLL.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection service)
+        {
+            service.AddTransient<IUserRepository, UserRepository>();
+            service.AddTransient<IImageRepository, ImageRepository>();
+            service.AddTransient<ITagRepository, TagRepository>();
+            return service;
+        }
+    }
+}
